c288960ed7c93c8bd584ec0fcff6e7f5b9a99c03
Shruti Sharma <shrutijangid5601@gmail.com>
2021-04-23 00:16:04 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Footer from './components/Footer';
 const App = () => {
   return (
     <div>
       <h1>Hello aliens this is DSC-DCE</h1>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './assets/css/footer.css';
+const Footer=()=>{
+    return(
+        <div>
+            <h1 className="footertxt">footer</h1>
+        </div>
+    );
+}
+export default Footer;
