122cd596c8a87341335e1085001e44a6d5c27bfc
Shruti Sharma <shrutijangid5601@gmail.com>
2021-04-23 16:25:56 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 const App = () => {
   return (
-    <div>
-      <h1>Hello aliens this is DSC-DCE</h1>
-      <Footer/>
-    </div>
+    <>
+     <Navbar/>
+      {/* <h1>Hello aliens this is DSC-DCE</h1>
+      <Footer/> */}
+    </>
   )
 }
 
