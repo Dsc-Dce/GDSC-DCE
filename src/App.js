3914d446ccb48abd847e20fb09039aac0d71e6a8
Aum Singhal <69421286+aum-singhal@users.noreply.github.com>
2021-04-23 12:30:15 +0530

    added Event component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Footer from './components/Footer';
+import Event from "./components/Event";
 const App = () => {
   return (
     <div>
       <h1>Hello aliens this is DSC-DCE</h1>
+      <Event />
       <Footer/>
     </div>
   )
