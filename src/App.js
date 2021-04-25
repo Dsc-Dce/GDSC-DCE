b8fc79158efb0773dd7a1e8ea779c34b3adf0761
Rajat Dash <apple@apples-MacBook-Air.local>
2021-04-25 16:10:33 +0530

    react route added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Event from "./components/Event";
+import { Route } from 'react-router-dom';
 const App = () => {
   return (
     <div>
 
       {/* <h1>Hello aliens this is DSC-DCE</h1> */}
       <Navbar/>
-      <Event />
+      <Route path="/events">
+        <Event />
+      </Route>
       <Footer/>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/navbar.css';
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import { NavLink} from 'react-router-dom';
 //logo
 import logo from './assets/images/DSC Light.png';
 
 const Navbar=()=>{
 return(
     <nav className="navbar navbar-expand-lg navbar-light ">
-  <a className="navbar-brand" href="#"><img src={logo} className="dsclogo"/></a>
+  <NavLink className="navbar-brand" to="/"><img src={logo} alt="dsc_logo" className="dsclogo"/></NavLink>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -19,16 +20,16 @@ return(
     <ul className="navbar-nav mr-auto">
       
       <li className="nav-item ">
-        <a className="nav-link active" href="#">About Us</a>
+        <NavLink className="nav-link active" to="/about">About Us</NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Team</a>
+        <NavLink className="nav-link" to="/team">Team</NavLink>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Events</a>
+        <NavLink className="nav-link" to="/events">Events</NavLink>
       </li>
       <li className="nav-item events-link">
-        <a className="nav-link " href="#">CoviHack'21</a>
+        <NavLink className="nav-link " to="/covihack">CoviHack'21</NavLink>
       </li>
     </ul>
   </div>
