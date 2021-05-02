6cf0d6c0b76a9574f514683dc22633a539ee830d
Aum Singhal <pycoder77@gmail.com>
2021-05-02 22:03:12 +0530

    added sponsors section

--- src/components/CoviHack.js
+++ src/components/CoviHack.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react'
 import CoviHack_Schedule from './CoviHack_Schedule'
+import Covihack_Sponsors from "./Covihack_Sponsors";
 
 const CoviHack = () => {
     return (
         <Fragment>
             <h1>this is covihack page.</h1>
             <CoviHack_Schedule/>
+            <Covihack_Sponsors />
         </Fragment>
     )
 }
---
+++ src/components/Covihack_Sponsors.js
@@ -0,0 +1,76 @@
+import React from "react";
+import "./assets/css/Covihack_Sponsors.css";
+import img from "./assets/images/About.jpg";
+import devfolio from "./assets/CoviHack Page assets/Sponsors/devfolio.png";
+import gfg from "./assets/CoviHack Page assets/Sponsors/gfg.png";
+import skillenza from "./assets/CoviHack Page assets/Sponsors/skillenza.png";
+import wolfram from "./assets/CoviHack Page assets/Sponsors/wolfram.png";
+import echoar from "./assets/CoviHack Page assets/Sponsors/echoAR.png";
+import progate from "./assets/CoviHack Page assets/Sponsors/progate.png";
+import replit from "./assets/CoviHack Page assets/Sponsors/replit.png";
+import nostarchpress from "./assets/CoviHack Page assets/Sponsors/nostarchpress.png";
+import taskade from "./assets/CoviHack Page assets/Sponsors/taskade.png";
+import cncf from "./assets/CoviHack Page assets/Sponsors/cncf.png";
+import jetbrains from "./assets/CoviHack Page assets/Sponsors/jetbrains.png";
+import givemycert from "./assets/CoviHack Page assets/Sponsors/givemycert.jpeg";
+import hoverrobotix from "./assets/CoviHack Page assets/Sponsors/hoverrobotix.png";
+
+
+const Covihack_Sponsors = () =>{
+    return(
+        <div className="sponsors">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-5 ">
+                        <div className="row-g">
+
+                            <div className="row">
+                                <div className="col gold-title">Gold</div>
+                            </div>
+
+                            <div className="row spons-row">
+                                <div className="col-5 spons-g" ><img src={devfolio} className="spons-row-img" /></div>
+                                <div className="col-5 spons-g" ><img src={gfg} className="spons-row-img" /></div>
+                            </div>
+
+                        </div>
+
+                        <div className="row-s">
+
+                            <div className="row">
+                                <div className="col silver-title">silver</div>
+                            </div>
+
+                            <div className="row spons-row row-1">
+                                <div className="col-4 spons-s" ><img src={skillenza} className="spons-row-img" /></div>
+                                <div className="col-4 spons-s" ><img src={wolfram} className="spons-row-img" /></div>
+                                <div className="col-4 spons-s" ><img src={echoar} className="spons-row-img" /></div>
+                            </div>
+
+                            <div className="row spons-row row-2">
+                                <div className="col-4 spons-s" ><img src={progate} className="spons-row-img" /></div>
+                                <div className="col-4 spons-s" ><img src={replit} className="spons-row-img" /></div>
+                                <div className="col-4 spons-s" ><img src={nostarchpress} className="spons-row-img" /></div>
+                            </div>
+
+                            <div className="row spons-row row-3">
+                                <div className="col-4 spons-s" ><img src={taskade} className="spons-row-img" /></div>
+                                <div className="col-4 spons-s" ><img src={cncf} className="spons-row-img" /></div>
+                                <div className="col-4 spons-s" ><img src={jetbrains} className="spons-row-img" /></div>
+                            </div>
+
+                            <div className="row spons-row row-4">
+                                <div className="col-6 spons-s" ><img src={givemycert} className="spons-row-img" /></div>
+                                <div className="col-6 spons-s" ><img src={hoverrobotix} className="spons-row-img" /></div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="col-md-7"><img src={img} className="spons-img" /></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Covihack_Sponsors;
