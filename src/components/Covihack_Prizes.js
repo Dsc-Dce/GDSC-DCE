a95e353e19782aa232bce44de6af058352e43f49
Rajat Dash <apple@apples-MacBook-Air.local>
2021-05-23 23:15:20 +0530

    special prize section added

--- src/components/Covihack_Prizes.js
+++ src/components/Covihack_Prizes.js
@@ -1,7 +1,10 @@
 import React, { Fragment } from 'react'
 import './assets/css/Covihack_Prizes.css'
 import PrizeCard from './PrizeCard';
-
+import celo from "./assets/CoviHack Page assets/Sponsors/Celo.png";
+import polygon from "./assets/CoviHack Page assets/Sponsors/polygon.png";
+import portis from "./assets/CoviHack Page assets/Sponsors/portis.jpg";
+import tezos from "./assets/CoviHack Page assets/Sponsors/tezos.jpg";
 const Covihack_Prizes = (props) => {
     return (
         <Fragment>
@@ -10,9 +13,57 @@ const Covihack_Prizes = (props) => {
                     <div className="container">
                         <h1 className="text-center">Prizes</h1>
                         <div className="row">
-                            <PrizeCard/>
-                            <PrizeCard/>
-                            <PrizeCard/>
+                            <PrizeCard 
+                            bgImage={polygon}
+                            heading="Polygon"
+                            desc={ <ul className="prizeCard_prizes">
+                            <li>
+                            ₹10,000 for the best hack built on Ethereum, or
+                            </li>
+                            <li>
+                            ₹15,000 for the best hack built on Ethereum + Polygon,
+                            </li>
+                            <li>
+                            Eligibility to apply for internship/full-time roles and seed funding of up to 5,000 USD for winners!
+                            </li>
+                            </ul>}
+                            />
+                           <PrizeCard 
+                           bgImage={portis}
+                            heading="Portis"
+                            desc={
+                                <ul className="prizeCard_prizes">
+                                    <li>
+                                    ₹15,000 for best Dapp built on Portis
+                                    </li>
+                                </ul>
+                            }
+                            />
+                            <PrizeCard 
+                            bgImage={tezos}
+                            heading="Tezos"
+                            desc={
+                                <ul className="prizeCard_prizes">
+                                    <li>
+                                    ₹20,000 for best Dapp built on Tezos
+                                    </li>
+                                    <li>
+                                    Continuity Grant opportunity for exceptional builders
+                                    </li>
+                                </ul>
+                            }
+                            />
+                            <PrizeCard 
+                            bgImage={celo}
+                            heading="celo"
+                            desc={
+                                <ul className="prizeCard_prizes">
+                                    <li>
+                                    Continuity Grant opportunity for exceptional builders
+                                    </li>
+                                </ul>
+                            }
+                            />
                         </div>
                     </div>
                 </div>
--- src/components/PrizeCard.js
+++ src/components/PrizeCard.js
@@ -1,18 +1,17 @@
 import React, { Fragment } from 'react'
-import tech from './assets/images/tech1.jpg';
 import pr from './assets/CoviHack Page assets/prizes/pngtree-first-prize-gold-trophy-icon-prize-gold-trophy-winner-first-prize-png-image_1934610.jpeg'
 
-const PrizeCard = (props) => {
+const PrizeCard = ({bgImage,heading,desc}) => {
     return (
         <Fragment>
-             <div className="col-lg-12 col-md-6 mt-4">
+             <div className="col-lg-4 col-md-6 mt-4">
                 <div className="one">
                     <div className="card">
-                        <div className="im1"><img src={tech} alt="im" /></div>
+                        <div className="im1"><img src={bgImage} alt="im" /></div>
                         <div className="im2"><img src={pr} alt="im2" /></div>
-                        <div className="main-text">
-                            <h2>heading</h2>
-                            <p>desc</p>
+                        <div className="main-texts">
+                            <h2>{heading}</h2>
+                            <p>{desc}</p>
                         </div>
                     </div>
                 </div>
